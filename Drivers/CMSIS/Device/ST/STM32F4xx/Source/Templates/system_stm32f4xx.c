1e93f5195839b9aff6a9329c9494c52262969798
Alex Burton <aburton@gmail.com>
2016-06-10 16:43:01 -0700

    removed dependancy on SetSysClock() function, works, but currently unstable serial for some reason, probably BRR (Baud Rate Register) configuration or delay, or something)

--- Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
+++ Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
@@ -200,7 +200,7 @@ void SystemInit(void)
   SCB->VTOR = FLASH_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal FLASH */
 #endif
 
-  SetSysClock();
+//  SetSysClock();
 }
 
 /**
--- cpu-arm_stm32f4xx.c
+++ cpu-arm_stm32f4xx.c
@@ -8,17 +8,48 @@
 #if defined TEACUP_C_INCLUDE && defined __ARM_STM32F411__
   
 #include "config_wrapper.h"
-
+#include "stm32f4xx_hal_conf.h"
 void cpu_init() {
   /**
     Enable all periphals.
   */
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 96;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
+
+
     // Enable power and clocking for all GPIO
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | 
                   RCC_AHB1ENR_GPIOBEN | 
                   RCC_AHB1ENR_GPIOCEN | 
                   RCC_AHB1ENR_GPIODEN | 
                   RCC_AHB1ENR_GPIOHEN;
+
+
+
 
 }
 
