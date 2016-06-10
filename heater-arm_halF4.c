dbd9dfc2e0ff4266006356d6c45f55775d7f8180
David <108nelson@gmail.com>
2016-06-10 12:50:09 -0700

    changed heater port to HAL so that it need _ARM_HALF4__ defined

--- heater-arm_halF4.c
+++ heater-arm_halF4.c
@@ -3,7 +3,7 @@
   \brief Manage heaters, including PID and PWM, ARM specific part.
 */
 
-#if defined TEACUP_C_INCLUDE && defined __ARM_STM32F411__
+#if defined TEACUP_C_INCLUDE && defined __ARM_HALF4__
 
 #include "cmsis-stm32f4xx.h"
 #include "pinio.h"
