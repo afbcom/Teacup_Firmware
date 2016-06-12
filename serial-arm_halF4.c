88d887abbbe1477d82f113fe2d429e7f3d1ccaec
Alex Burton <aburton@gmail.com>
2016-06-12 13:04:44 -0700

    serial works, not sure why

--- serial-arm_halF4.c
+++ serial-arm_halF4.c
@@ -26,23 +26,15 @@
 #define SERIAL_TIMEOUT (0xFFFF)
 
 UART_HandleTypeDef huart;
-	
+
 void serial_init()
 {
-	
-//	RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+
 	GPIO_InitTypeDef  igpio;
-	
-		// Enable TX/RX clock (GPIOA)
 	__HAL_RCC_GPIOA_CLK_ENABLE();
-    // Enable USART2 clock
+//	Enable USART2 clock
 	__HAL_RCC_USART2_CLK_ENABLE(); //STM32F401 specific
-	
-	
-	// AF 4bits per channel
-    // Alternate functions from DM00115249.pdf datasheet (page 47; table 9)
-//	#undef GPIO_PIN
-  //  #define GPIO_PIN(number) (GPIO_PIN_ ## number)
+
 
 	igpio.Pin = GPIO_PIN_2 | GPIO_PIN_3;
 	igpio.Mode = GPIO_MODE_AF_PP;
@@ -51,22 +43,17 @@ void serial_init()
 	igpio.Alternate = GPIO_AF7_USART2; //STM32F401 specific
 	HAL_GPIO_Init(GPIOA, &igpio);
 
-//	#undef GPIO_PIN
-	
-	/* Configure the UART */
 
-    /* 19.3.4 Fractional baud rate generation => reference manual for STM32F411
-    Set BRR for 115,200 Hz 
-    div = 48MHz/(16*BAUD)
-    Mantisse = int(div) << 8
-    Divisor = int((div - int(div))*16)
-    BRR = Mantisse + Divisor
-    */
+
     #if !defined BAUD
     #define BAUD 115200
     #endif
     
-  huart.Instance = USART2;
+//  huart.Instance = USART2;
+
+
+	huart.Instance = USART2;
+
 	huart.Init.BaudRate = (uint32_t)(BAUD);
 	huart.Init.WordLength = UART_WORDLENGTH_8B;
 	huart.Init.StopBits = UART_STOPBITS_1;
@@ -74,42 +61,47 @@ void serial_init()
 	huart.Init.Mode = UART_MODE_TX_RX;
 	huart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
 	huart.Init.OverSampling = UART_OVERSAMPLING_16;
-	
+
 	HAL_UART_Init(&huart);
 
+
 }
 
-/** Check wether characters can be read.
 
-  Other than the AVR implementation this returns not the number of characters
-  in the line, but only wether there is at least one or not.
-*/
-uint8_t serial_rxchars(void) {  
-  return huart.RxXferCount;
+
+uint8_t serial_rxchars(void) {
+  return USART2->SR & USART_SR_RXNE;
 }
 
-// /** Read one character.*/
+ /** Read one character.*/
+
 uint8_t serial_popchar(void) {
-  uint8_t c[2] = {0,0};
-  HAL_UART_Receive(&huart, c, 1 , SERIAL_TIMEOUT);
+   uint8_t c = 0;
+   HAL_UART_Receive(&huart, &c, 1 , SERIAL_TIMEOUT);
 
-  return c;
+   return c;
 }
 
-/** Check wether characters can be written
-  ***STM-NUCLEO-PORT: THIS IS NOT NEEDED ***
+
+
+
+ uint8_t serial_txchars(void) {
+   return huart.TxXferCount;
+}
+
+
+/** Send one character.
 */
-uint8_t serial_txchars(void) {
-  return huart.TxXferCount;
-}
 
-
-// //TODO Change or rethink delays
 void serial_writechar(uint8_t data) {
-	
+  if ( !serial_txchars())       // Queue full?
+
     delay_us((1000000 / BAUD * 10) + 7);
 	HAL_UART_Transmit(&huart, &data, 1, SERIAL_TIMEOUT);
 }
+
+
+
 
 
 
