f9ca5338ed9d3abb2829b4c71bfb780b24a5070a
Alex Burton <aburton@gmail.com>
2016-06-17 09:15:34 -0700

    tweaking cpu init to see if HSE Bypass from onboard STLink works.

--- cpu-arm_stm32f4xx.c
+++ cpu-arm_stm32f4xx.c
@@ -20,7 +20,9 @@ void cpu_init() {
   __HAL_RCC_PWR_CLK_ENABLE();
 
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+#define __HSI__
 
+#if defined __HSI__
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = 16;
@@ -45,12 +47,33 @@ void cpu_init() {
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
   HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3);
 
+#else
+
+RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 8;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3);
+
+#endif
 
     // Enable power and clocking for all GPIO
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | 
-                  RCC_AHB1ENR_GPIOBEN | 
-                  RCC_AHB1ENR_GPIOCEN | 
-                  RCC_AHB1ENR_GPIODEN | 
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN |
+                  RCC_AHB1ENR_GPIOBEN |
+                  RCC_AHB1ENR_GPIOCEN |
+                  RCC_AHB1ENR_GPIODEN |
                   RCC_AHB1ENR_GPIOHEN;
 
 
